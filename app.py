2bc28cb56eb02f6a2a463d838855de33db24f112
CMEONE <contact@kabirwearsato.ga>
2021-03-27 09:42:30 -0700

    Template for logins

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def add():
 
 @app.route('/register')
 def register():
-    return render_template('elements.html')
+    return render_template('register.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('elements.html')
+    return render_template('login.html')
