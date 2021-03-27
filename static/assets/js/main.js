85afc38a10f3e598b9e31f89753bc036588a9535
CMEONE <contact@kabirwearsato.ga>
2021-03-27 11:11:42 -0700

    Preloader for auth

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -3,12 +3,12 @@
   
 
 /* 1. Proloder */
-    $(window).on('load', function () {
-      $('#preloader-active').delay(450).fadeOut('slow');
-      $('body').delay(450).css({
-        'overflow': 'visible'
-      });
-    });
+    // $(window).on('load', function () {
+    //   $('#preloader-active').delay(450).fadeOut('slow');
+    //   $('body').delay(450).css({
+    //     'overflow': 'visible'
+    //   });
+    // });
 
 /* 2. sticky And Scroll UP */
     $(window).on('scroll', function () {
