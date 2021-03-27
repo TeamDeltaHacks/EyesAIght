9f1ac43ab9d75f3eb5936d42c00e30b69412710b
CMEONE <contact@kabirwearsato.ga>
2021-03-27 09:07:41 -0700

    Import template

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+	
+@app.route('/records')
+def view_records():
+	return render_template('elements.html')
+		
+@app.route('/records/<record>')
+def records(record):
+	print(record)
+	return render_template('elements.html')
+		
+@app.route('/add')
+def add():
+	return render_template('elements.html')
+	
+@app.route('/register')
+def register():
+	return render_template('elements.html')
+	
+@app.route('/login')
+def login():
+	return render_template('elements.html')
