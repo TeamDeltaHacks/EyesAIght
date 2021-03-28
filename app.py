2ce52e96a4c6bd39e05b36bbf4e1683fc5ad8ec6
CMEONE <contact@kabirwearsato.ga>
2021-03-27 19:05:34 -0700

    DR detection

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ import tensorflow as tf
 import keras
 from keras.models import load_model
 import pandas as pd
+import numpy as np
 import cv2
+import urllib
 from summarizer import Summarizer  # BERT model
 import sklearn as sk
 from sksurv.linear_model import CoxnetSurvivalAnalysis
 
 survival_TR = load('coxnetTR.joblib')
 survival_UT = load('coxnetUT.joblib')
+
+dr_weights = load_model("dr_weights.h5")
 
 app = Flask(__name__)
 
@@ -39,7 +43,35 @@ def add():
     else:
         json = request.json
         if(json["type"] == 1):
-            pass
+            if("image" not in json or json["image"] == ""):
+                return '{"type":"error","response":"Image field must not be left blank."}'
+
+            req = urllib.request.urlopen(json["image"])
+            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+            image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+            image = cv2.resize(image, (224, 224))
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            image_tensor = tf.convert_to_tensor(image, dtype=tf.float32)
+            image_tensor = tf.expand_dims(image_tensor, 0)
+            # image_tensor = tf.expand_dims(image_tensor, 2)
+            result = dr_weights.predict(image_tensor)[0]
+            highestVal = 0
+            highestIndex = 0
+            for index, value in enumerate(result):
+                if(value > highestVal):
+                    highestVal = value
+                    highestIndex = index
+            output = "No diabetic retinopathy."
+            if highestIndex == 1:
+                output = "Mild diabetic retinopathy."
+            elif highestIndex == 2:
+                output = "Moderate diabetic retinopathy."
+            elif highestIndex == 3:
+                output = "Severe diabetic retinopathy."
+            elif highestIndex == 4:
+                output = "Proliferative diabetic retinopathy."
+
+            return '{"type":"success","response":"' + output + '"}'
         elif(json["type"] == 2):
             pass
         elif(json["type"] == 3):
